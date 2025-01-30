8924de25383046b9256e74cec4fb2bbd2895d343
Vincent-Le-Grand <v.w.legrand@outlook.com>
2025-01-30 21:24:04 +0100

    edit brian is cool

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("hallo")
+const print50 =  () => {
+    test.innerHTML = "brain is cool"
+}
+const btn = document.getElementById("btn");
+btn.addEventListener("click", print50)
+const test = document.getElementById("test");
